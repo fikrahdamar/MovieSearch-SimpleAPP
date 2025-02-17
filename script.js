f38ffc25c10f67d1b1245ced40feff2391647544
fikrahdamar <fikrahdamar7@gmail.com>
2025-02-17 15:46:50 +0700

    [Edit file javascript]

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("test")
+$.ajax ({
+    url : 'http://www.omdbapi.com/?apikey=e179c17d&s=harry potter',
+    success : results => {
+        console.log(results);
+    },
+    error : (e) =>{
+        console.log(e.responseText);
+    }
+});
